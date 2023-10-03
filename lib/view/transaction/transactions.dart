d588d73841f2b194a29107a61d9b0486ebfd89bb
muhammed safvan <sajusajuptl@gmail.com>
2023-10-03 20:21:37 +0530

    drawer added

--- lib/view/transaction/transactions.dart
+++ lib/view/transaction/transactions.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:money_management_app/controller/core/constant.dart';
 import 'package:money_management_app/view/transaction/widgets/custom_cliper_shape.dart';
+import 'package:money_management_app/view/transaction/widgets/money_status_bar.dart';
 
 class TrasactionsScreen extends StatelessWidget {
   const TrasactionsScreen({super.key});
@@ -20,13 +21,14 @@ class TrasactionsScreen extends StatelessWidget {
                   height: 230,
                   width: double.infinity,
                   decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [
-                        CustomColors.commonClr,
-                        CustomColors.gradientSecond
-                      ])),
+                    gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          CustomColors.commonClr,
+                          CustomColors.gradientSecond
+                        ]),
+                  ),
                   child: Container(
                     padding:
                         const EdgeInsets.only(left: 30, right: 30, top: 30),
@@ -34,6 +36,13 @@ class TrasactionsScreen extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
+                        IconButton(
+                          onPressed: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                          icon: Icon(Icons.menu_rounded,
+                              color: CustomColors.kwhite),
+                        ),
                         Text(
                           "WALLET APP",
                           style: CustomFuction.style(
@@ -53,86 +62,11 @@ class TrasactionsScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Positioned(
+              const Positioned(
                 bottom: 0,
                 left: 30,
                 right: 30,
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
-                  height: 150,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.blue,
-                          spreadRadius: -4.0,
-                          blurRadius: 10.0,
-                        )
-                      ]),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Your Balance',
-                        style: CustomFuction.style(
-                            fontWeight: FontWeight.bold,
-                            size: 14,
-                            color: CustomColors.commonClr),
-                      ),
-                      Text(
-                        ' \$500.00',
-                        style: CustomFuction.style(
-                            fontWeight: FontWeight.bold, size: 18),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Divider(color: Colors.grey.withAlpha(50)),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Income',
-                                  style: CustomFuction.style(
-                                      fontWeight: FontWeight.bold, size: 14),
-                                ),
-                                Text(
-                                  '\$50.00',
-                                  style: CustomFuction.style(
-                                      fontWeight: FontWeight.bold,
-                                      size: 18,
-                                      color: CustomColors.kgreen),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Expences',
-                                  style: CustomFuction.style(
-                                      fontWeight: FontWeight.bold, size: 14),
-                                ),
-                                Text(
-                                  '\$45.00',
-                                  style: CustomFuction.style(
-                                      fontWeight: FontWeight.bold,
-                                      size: 18,
-                                      color: CustomColors.kred),
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
+                child: MoneyStatusBar(),
               )
             ],
           ),
