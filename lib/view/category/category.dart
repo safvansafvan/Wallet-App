6540189b1ce7a92253bd6c50411ca448a0d9bad1
muhammed safvan <sajusajuptl@gmail.com>
2023-10-10 11:20:36 +0530

    categorys added and tab bar added income and expence

--- lib/view/category/category.dart
+++ lib/view/category/category.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:money_management_app/controller/core/constant.dart';
+import 'package:money_management_app/view/category/widget/expence.dart';
+import 'package:money_management_app/view/category/widget/income.dart';
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
 
   @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("data"),
+    return Column(
+      children: [
+        AppBar(
+          backgroundColor: CustomColors.commonClr,
+          leading: IconButton(
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              icon: Icon(
+                Icons.menu,
+                color: CustomColors.kwhite,
+              )),
+          title: Text(
+            'STATICS',
+            style: CustomFuction.style(
+                fontWeight: FontWeight.w600,
+                size: 17,
+                color: CustomColors.kwhite),
+          ),
+          centerTitle: true,
+        ),
+        TabBar(
+          controller: controller,
+          labelStyle:
+              CustomFuction.style(fontWeight: FontWeight.bold, size: 16),
+          tabs: [
+            Tab(
+              text: 'Income',
+            ),
+            Tab(
+              text: 'Expence',
+            ),
+          ],
+        ),
+        Expanded(
+          child: TabBarView(
+              controller: controller,
+              children: [IncomeTabBar(), ExpenceTabBar()]),
+        )
+      ],
     );
   }
 }
---
+++ lib/view/category/widget/income.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:money_management_app/controller/core/constant.dart';
+
+class IncomeTabBar extends StatelessWidget {
+  const IncomeTabBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Container(
+            height: 60,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(
+                color: CustomColors.containerColor,
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(color: CustomColors.kblue.withAlpha(100)),
+                boxShadow: [BoxShadow(color: CustomColors.kblue)]),
+            child: Center(
+              child: ListTile(
+                title: Text(
+                  'IncomeCategory$index',
+                  style: CustomFuction.style(
+                      fontWeight: FontWeight.w600, size: 16),
+                ),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.delete_outline_outlined,
+                      color: CustomColors.kred,
+                    )),
+              ),
+            ),
+          ),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+}
