45a58e9509b488d38d6ed254f40c843089344d84
muhammed safvan <sajusajuptl@gmail.com>
2023-10-11 16:21:58 +0530

    splash screen added

---
+++ lib/view/splash/screen_loding.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:money_management_app/controller/core/constant.dart';
+import 'package:money_management_app/view/auth/login.dart';
+
+class ScrennLoadingScreen extends StatefulWidget {
+  const ScrennLoadingScreen({super.key});
+
+  @override
+  State<ScrennLoadingScreen> createState() => _ScrennLoadingScreenState();
+}
+
+class _ScrennLoadingScreenState extends State<ScrennLoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigation(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.commonClr,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/logo/applogo.png',
+                        height: 150, width: 150),
+                    Text(
+                      'W A L L E T  A P P',
+                      style: CustomFuction.style(
+                        fontWeight: FontWeight.bold,
+                        size: 15,
+                        color: CustomColors.kwhite,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Lottie.asset('assets/animation/app_loading.json',
+                      height: 50, width: 50),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> navigation(context) async {
+    await Future.delayed(const Duration(seconds: 3), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+  }
+}
